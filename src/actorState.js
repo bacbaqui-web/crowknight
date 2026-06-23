81bccacf9e675ee16ae9120a63b3321d48c47abe
bacbaqui-web <cockrott@gmail.com>
2026-06-24 01:57:35 +0900

    refactor: extract actor state helpers

---
+++ src/actorState.js
@@ -0,0 +1,21 @@
+export function resetPlayerActionState(player) {
+  player.attackTime = 0;
+  player.jumpAttackTime = 0;
+  player.dashTime = 0;
+  player.attackCooldown = 0;
+  player.attackCarrySpeed = 0;
+  player.hurtTime = 0;
+  player.guardActive = false;
+  player.guardHits = 0;
+  player.guardBlockTime = 0;
+  player.guardBreakTime = 0;
+  player.guardLockedUntilRelease = false;
+}
+
+export function updatePostRollInvulnerability(actor) {
+  const isRolling = actor.player.isRolling;
+  if (actor.wasRolling && !isRolling) {
+    actor.invulnTime = Math.max(actor.invulnTime, actor.tuning.invulnerability.rollEnd);
+  }
+  actor.wasRolling = isRolling;
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { PuppetPlayer } from './puppetPlayer.js';
 import { defaultTuningFor, syncActorHealthCapacity } from './actorTuning.js';
 import { drawActorShadow, drawHealthMeter } from './actorHudRenderer.js';
+import { resetPlayerActionState, updatePostRollInvulnerability } from './actorState.js';
 import {
   defaultEffectImageKey,
   defaultEffectSize,
@@ -403,28 +404,6 @@ function updateStunnedActor(actor, dt) {
   player.jumpHoldTime = 0;
   player.glideActive = false;
   player.updateState();
-}
-
-function resetPlayerActionState(player) {
-  player.attackTime = 0;
-  player.jumpAttackTime = 0;
-  player.dashTime = 0;
-  player.attackCooldown = 0;
-  player.attackCarrySpeed = 0;
-  player.hurtTime = 0;
-  player.guardActive = false;
-  player.guardHits = 0;
-  player.guardBlockTime = 0;
-  player.guardBreakTime = 0;
-  player.guardLockedUntilRelease = false;
-}
-
-function updatePostRollInvulnerability(actor) {
-  const isRolling = actor.player.isRolling;
-  if (actor.wasRolling && !isRolling) {
-    actor.invulnTime = Math.max(actor.invulnTime, actor.tuning.invulnerability.rollEnd);
-  }
-  actor.wasRolling = isRolling;
 }
 
 function updateRespawningEnemy(actor, dt) {
